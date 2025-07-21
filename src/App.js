7b300540b807de0960a9af0855ccc065aa635bee
SaiKiran93814 <teppali123@gmail.com>
2025-07-21 13:35:12 +0530

    Completed assignment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const handleClick = () => {
+    setShowParagraph(true);
+  };
+
+  return (
+    <div>
+      <button id="click" onClick={handleClick}>
+        Click Me
+      </button>
+
+      {showParagraph && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default App;
